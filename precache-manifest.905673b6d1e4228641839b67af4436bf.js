dc2815c55e0115cf5c9738d09a9ec70f6df9af68
mephi007 <roysumit949@gmail.com>
2021-12-10 17:51:49 +0530

    Updates

---
+++ precache-manifest.905673b6d1e4228641839b67af4436bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1ad1b3d17dc12f2d411dc7fada92e1",
+    "url": "/Swaggster/index.html"
+  },
+  {
+    "revision": "47682bc5907e4871a572",
+    "url": "/Swaggster/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "444213687ea36d56b9dc",
+    "url": "/Swaggster/static/js/2.d8b92b78.chunk.js"
+  },
+  {
+    "revision": "56e9a581d0dafe53e794d1ab848e11b8",
+    "url": "/Swaggster/static/js/2.d8b92b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1c517592112d618ba4",
+    "url": "/Swaggster/static/js/3.5f363b4d.chunk.js"
+  },
+  {
+    "revision": "47682bc5907e4871a572",
+    "url": "/Swaggster/static/js/main.73487514.chunk.js"
+  },
+  {
+    "revision": "8b7079483c3775db47e8",
+    "url": "/Swaggster/static/js/runtime-main.c3433c29.js"
+  }
+]);
